934b0f50c967c924541c5143a4df19de847b50c4
Noah Altunian <noah@altun.cc>
2025-02-11 17:11:26 -0800

    Replace err(3) with errx(3) in test_wg.c

--- server/tests/test_wg.c
+++ server/tests/test_wg.c
@@ -24,21 +24,21 @@ main()
 
     /* Check if we're running as root */
 	if (getuid() != 0)
-		err(1, "must run as root");
+		errx(1, "must run as root");
 
     /*
      * 1
      */
 	printf("Test open wg0 interface...\n");
 	if ((ret = wg_open_iface(&wg, "wg0")) != FW_OK)
-		err(1, "wg_open_iface: failed to open WireGuard interface");
+		errx(1, "wg_open_iface: failed to open interface");
 
     /*
      * 2
      */
 	printf("Test create wg0 interface...\n");
 	if ((ret = wg_create_iface(&wg)) != FW_OK)
-		err(1, "wg_create_iface: failed to create WireGuard interface");
+		errx(1, "wg_create_iface: failed to create interface");
 
     /*
      * 3
@@ -48,7 +48,7 @@ main()
 	iface.i_flags = WG_INTERFACE_HAS_PORT;
 	iface.i_port = 51820;
 	if ((ret = wg_set_iface(&wg, &iface)) != FW_OK)
-		err(1, "wg_set_iface: failed to configure interface with port");
+		errx(1, "wg_set_iface: failed to configure interface");
 
     /*
      * 4
@@ -56,14 +56,14 @@ main()
 	printf("Test get listen port...\n");
 	memset(&iface, 0, sizeof(iface));
 	if ((ret = wg_get_iface(&wg, &iface)) != FW_OK)
-		err(1, "wg_get_iface: failed to get interface configuration");
+		errx(1, "wg_get_iface: failed to get interface configuration");
 
     /*
      * 5 -- destroy interface before closing it to prevent EEXIST error
      */
 	printf("Test destroy wg0 interface...\n");
 	if ((ret = wg_destroy_iface(&wg)) != FW_OK)
-		err(1, "wg_destroy_iface: failed to destroy interface");
+		errx(1, "wg_destroy_iface: failed to destroy interface");
 
     /*
      * 6
